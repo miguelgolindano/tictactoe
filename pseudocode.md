856f32bad40f86fe2d6c4d994d45210ae9e9329a
miguel <golindanomejias@gmail.com>
2024-12-04 04:28:39 -0400

    making the logic to determine who is the winner comparing the user and the computer choice

---
+++ pseudocode.md
@@ -0,0 +1,8 @@
+create a rock paper scissor game
+
+create the logic for the computer to choose a between rock paper scissor
+create the logic for capture the user selection
+evaluate if the computer won
+display the result
+keep the score of the user and the computer
+create the logic letting the program know how many rounds have passed
--- script.js
+++ script.js
@@ -1,13 +1,76 @@
 
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+// this function let the computer choice using a random number
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 100)
-  return computerChoice
+  let b = Math.floor(Math.random() * 100)
+  return b
 }
 
+//this function get the choice of the user and return it lowercase
 function getHumanChoice() {
-  let humanChoice = prompt('Your Choice', 'Here you can enter your choice')
+  let a = prompt('Your Choice', 'Here you can enter your choice')
+  return a.toLowerCase()
 }
 
+function playRound(humanChoice, computerChoice) {
+  
+  switch(humanChoice) {
+
+    case 'paper':      
+      if (computerChoice == 'paper') {
+        console.log('It\'s a draw!');
+      }
+      else if (computerChoice == 'rock') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+      }
+
+      else if (computerChoice == 'scissor') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++
+      }
+      break;
+  
+    case 'rock':
+      if (computerChoice == 'paper') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++
+      }
+      else if (computerChoice == 'rock') {
+        console.log('It\'s a draw!');
+      }
+
+      else if (computerChoice == 'scissor') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+      }
+      break;
+
+    case 'scissor':
+      if (computerChoice == 'paper') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+      }
+      
+      else if (computerChoice == 'rock') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++        
+      }
+
+      else if (computerChoice == 'scissor') {
+        console.log('It\'s a draw!');
+      }
+
+      break;
+  }
+
+}
+
+/*
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+*/
